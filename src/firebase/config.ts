b24e55a62d8f6080e850747a658ef71b281baccb
Sakshi Singh <harrypotter.sak20@gmail.com>
2026-03-21 11:23:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7644657607-c44a1",
+  "appId": "1:796941231384:web:ec22b3e6e3152ee63bea6d",
+  "apiKey": "AIzaSyBZ1wrbOooNvSIKhGkVwc58YqZMwZjKM3k",
+  "authDomain": "studio-7644657607-c44a1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "796941231384"
+};
